5f9c480b324766370ec405ad920eef55456b47a7
Laiser399 <Laiser399@mail.ru>
2020-11-27 22:59:37 +0300

    init commit

---
+++ CNLab4 Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNLab4_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CNLab4 Server/Server.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace CNLab4_Server
+{
+    class Server
+    {
+        private TcpListener _listener;
+        private bool _isStarted = false;
+
+
+        public Server()
+        {
+            _listener = new TcpListener(IPAddress.Any, 59399);
+        }
+
+        public async void Start()
+        {
+            if (_isStarted)
+                return;
+            _isStarted = true;
+
+            _listener.Start();
+
+            while (_isStarted)
+            {
+                TcpClient client = await _listener.AcceptTcpClientAsync();
+                OnClientAcceptedAsync(client);
+            }
+        }
+
+        public void Stop()
+        {
+            if (!_isStarted)
+                return;
+            _isStarted = false;
+        }
+
+        private async void OnClientAcceptedAsync(TcpClient client)
+        {
+            using (client)
+            {
+                NetworkStream stream = client.GetStream();
+
+                JObject obj = await stream.ReadJObjectAsync();
+                
+                switch (obj.Value<string>("type"))
+                {
+                    case "register_torrent":
+                        {
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
